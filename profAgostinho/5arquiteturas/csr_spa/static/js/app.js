c97865f4d59f7bdb1c97f775eef5a8f474105dc4
João Pedro Marques <joaolupyo>
2025-11-30 06:25:03 -0300

    mais ajustes

--- profAgostinho/5arquiteturas/csr_spa/static/js/app.js
+++ profAgostinho/5arquiteturas/csr_spa/static/js/app.js
@@ -1,1 +1,8 @@
-app.js
+fetch("/posts")
+  .then(res => res.json())
+  .then(data => {
+    const div = document.getElementById("posts");
+    data.forEach(post => {
+      div.innerHTML += `<h2>${post.title}</h2><p>${post.text}</p><hr>`;
+    });
+  });
--- profAgostinho/5arquiteturas/ssg/build.py
+++ profAgostinho/5arquiteturas/ssg/build.py
@@ -1,1 +1,17 @@
-build.py
+from jinja2 import Environment, FileSystemLoader
+
+POSTS = [
+    {"title": "A essência do street skate", "text": "O street skate é sobre criatividade e liberdade."},
+    {"title": "A evolução das manobras", "text": "As manobras cresceram junto com a cultura do skate."},
+    {"title": "Por que o skate é estilo de vida", "text": "Skate é atitude, comunidade e expressão."}
+]
+
+env = Environment(loader=FileSystemLoader("templates"))
+template = env.get_template("base.html")
+
+html = template.render(posts=POSTS)
+
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write(html)
+
+print("SSG gerado → index.html")
